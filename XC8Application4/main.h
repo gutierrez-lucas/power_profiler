a90f986c7dd78233284d20abbc9e7326ce4df290
Lucas Gutierrez <gutierrezjuanlucas@gmail.com>
2023-07-28 13:33:11 -0300

    pragmas moved

--- XC8Application4/main.h
+++ XC8Application4/main.h
@@ -3,9 +3,11 @@
 
 #define F_CPU 16000000UL	// 16MHz clock 
 
-#define PRINT_SENSOR_DATA
-#ifdef PRINT_SENSOR_DATA	// this is for python plotter usage
-#undef PRINT_SYSTEM_INFO	// this is for dev debug info, mutually exclusive with the plotter. 
+//#define PRINT_SYSTEM_INFO  // dev debug mode, not external usage
+
+#define PRINT_SENSOR_DATA	//external usage mode
+#ifdef PRINT_SENSOR_DATA	
+#undef PRINT_SYSTEM_INFO	// mutually exclusive with the plotter. 
 char main_print_buff[50];
 #endif
 
